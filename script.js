c1dee3f210d05e10c7923bf7a4699fe90be05f7f
ChandanWebDevelops <98172240+ChandanWebDevelops@users.noreply.github.com>
2022-03-26 10:04:59 +0545

    Update script.js

--- script.js
+++ script.js
@@ -175,9 +175,9 @@ save2.addEventListener('click',function (){
 
 function newFunc(){
 main = document.getElementsByClassName("navBar")[0];
+wid = window.innerWidth;
 
-
-main.style.width = window.innerWidth-60+"px; !important";
+main.style.width = `${wid-60}px`
 hi = document.querySelectorAll(".CodeMirror-vscrollbar, .CodeMirror-hscrollbar, .CodeMirror-scrollbar-filler, .CodeMirror-gutter-filler")
 for(i=0; i<hi.length; i++){
     hi[i].style.zIndex = "0";
